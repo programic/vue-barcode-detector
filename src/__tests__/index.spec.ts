402a19b859df24e76bb75e8af1a93df2db61b07e
Jesse Koerhuis <43340526+jessekoerhuis@users.noreply.github.com>
2024-11-29 15:28:41 +0100

    Develop (#3)
    * Feature/setup (#1)
    
    * feat(setup): Initial commit
    
    * feat(setup): Removed IDE files and added IDE files to gitignore file
    
    * feat(setup): Changed URL for the repository in the package.json file
    
    * Feature/setup (#2)
    
    * feat(setup): Initial commit
    
    * feat(setup): Removed IDE files and added IDE files to gitignore file
    
    * feat(setup): Changed URL for the repository in the package.json file
    
    * feat(setup): Added barcode scanner composable
    
    * Replaced Jest testing framework with Vitest
    
    * Updated README and CHANGELOG

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,68 @@
+import { describe, expect, it } from 'vitest';
+import { flushPromises } from '@vue/test-utils';
+import useBarcodeDetector, { type ScannedBarcodeData } from '../index';
+
+/**
+ * @vitest-environment jsdom
+ */
+describe('the barcode detector', () => {
+  it('should listen to the keydown events', async () => {
+    const barcodeDetector = useBarcodeDetector();
+    let barcodeValue = null;
+
+    barcodeDetector.listen((barcodeData: ScannedBarcodeData): void => {
+      barcodeValue = barcodeData.value;
+    });
+
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a', code: 'a' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'd', code: 'd' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: '4', code: '4' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: '6', code: '6' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'f', code: 'f' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: '2', code: '2' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: '1', code: '1' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'h', code: 'h' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'x', code: 'x' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter' }));
+
+    expect(barcodeValue).toBe('ad46f21hx');
+  });
+
+  it('should reset the values when no Enter key was pressed', async () => {
+    const barcodeDetector = useBarcodeDetector();
+    let barcodeValue = null;
+
+    barcodeDetector.listen((barcodeData: ScannedBarcodeData): void => {
+      barcodeValue = barcodeData.value;
+    });
+
+    expect(barcodeValue).toBe(null);
+
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'x', code: 'x' }));
+
+    expect(barcodeValue).toBe(null);
+
+    await flushPromises();
+
+    expect(barcodeValue).toBe(null);
+  });
+
+  it('should reset data after Enter key pressed', async () => {
+    const barcodeDetector = useBarcodeDetector();
+    let barcodeValue = null;
+
+    barcodeDetector.listen((barcodeData: ScannedBarcodeData): void => {
+      barcodeValue = barcodeData.value;
+    });
+
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'b', code: 'b' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: '8', code: '8' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'u', code: 'u' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'z', code: 'z' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a', code: 'a' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter' }));
+
+    expect(barcodeValue).not.toEqual(barcodeDetector.barcode.value);
+    expect(barcodeDetector.barcode.value).toEqual('');
+  });
+});
